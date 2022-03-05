c2fd8e1c6d4b7baf2acaee052012cd9ef410459b
hachiyomo <yomohachi09@gmail.com>
2022-03-05 06:35:45 +0000

    favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,7 +9,7 @@ before_action :require_user_logged_in
   end
 
   def destroy
-    review = Review.find(params[:review_id])
+    review = Review.find_by(params[:review_id])
     current_user.unfavorite(review)
     flash[:success] = 'お気に入りを解除しました。'
     redirect_back(fallback_location: root_path)
